7782ececf3242bab68333a4c57545b2c56967d16
suqingdong <suqingdong1114@gmail.com>
2021-04-28 14:07:56 +0800

    add venn

---
+++ src/venn/README.md
@@ -0,0 +1,26 @@
+### Examples
+```bash
+Rscript venn.R -h
+
+Usage: Rscript venn.R [Options]
+
+Options:
+	-i INFILE, --infile=INFILE
+		The input file.
+
+	-o OUTFILE, --outfile=OUTFILE
+		The output file.
+
+	--height=HEIGHT
+		The height of image[default=1000].
+
+	--width=WIDTH
+		The width of image[default=1000].
+
+	--main=MAIN
+		The main title of image[default=""].
+
+	-h, --help
+		Show this help message and exit
+
+```
---
+++ src/venn/venn.R
@@ -0,0 +1,51 @@
+suppressWarnings(library(optparse))
+suppressWarnings(library(VennDiagram))
+
+
+option_list <- list(
+  make_option(c('-i', '--infile'), dest='infile', default=NULL, help='The input file.'),
+  make_option(c('-o', '--outfile'), dest='outfile', default=NULL, help='The output file.'),
+  make_option(c('--height'), dest='height', type='integer', default=1000, help='The height of image[default=%default].'),
+  make_option(c('--width'), dest='width', type='integer', default=1000, help='The width of image[default=%default].'),
+  make_option(c('--main'), dest='main', default='', help='The main title of image[default="%default"].')
+)
+
+parser <- OptionParser(option_list, usage="Rscript %prog [Options]")
+opts <- parse_args(parser)
+
+library(showtext)
+showtext_auto()
+
+infile <- opts$infile
+outfile <- opts$outfile
+height <- opts$height
+width <- opts$width
+main <- opts$main
+
+options(bitmapType = 'cairo')
+
+if ( is.null(infile) || is.null(outfile) ) {
+  cat('Usage: Rscript venn.R -h for more help information.\n')
+  quit()
+}
+
+
+data <- read.table(infile, header=T, sep='\t', fileEncoding='UTF-8')
+
+venn.diagram(
+  x = as.list(data), 
+  filename = outfile,
+  imagetype = 'png',
+  height = height,
+  width = width,
+  fill = rainbow(length(data)), 
+  cex = 0.5,
+  col ="transparent",
+  alpha = 0.5,
+  margin = 0.1,
+  resolution = 300,
+  cat.cex = 0.6,
+  cat.col = rainbow(length(data)),
+  main = main
+
+)
